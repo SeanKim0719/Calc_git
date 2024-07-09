639ecdcb94dd54775715f75a3dc96c07e3ee8fae
JungSu Kim <rlawjdtn0719@gmail.com>
2024-07-10 01:02:38 +0900

    초기 커밋.

---
+++ Calc_git/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        calc.display();
+    }
+}
+
+class Calculator
+{
+    public void display()
+    {
+        Console.WriteLine("Calculate is booting");
+    }
+}
