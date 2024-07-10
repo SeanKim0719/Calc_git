4a6f7efec273726487f475acfed2dacde53de7a3
NDH <ndhphysics@gamil.com>
2024-07-10 20:47:58 +0900

    사칙연산 인터페이스 & 메서드 구현

--- Calc_git/Program.cs
+++ Calc_git/Program.cs
@@ -11,10 +11,45 @@ class Program
     }
 }
 
-class Calculator
+//the Four Fundamental Arithmetic Operations
+interface FFAO
+{
+    int Add(int num1, int num2);
+    int Sub(int num1, int num2);
+    int Mul(int num1, int num2);
+    int Div(int num1, int num2);
+
+}
+
+class Calculator: FFAO
 {
     public void display()
     {
         Console.WriteLine("Calculate is booting");
+    }
+
+    int FFAO.Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    int FFAO.Sub(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+    int FFAO.Mul(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+    int FFAO.Div(int num1, int num2)
+    {
+        try
+        {
+            return num1 / num2;
+        }
+        catch(DivideByZeroException)
+        {
+            Console.WriteLine("0으로는 나눌 수 없습니다.");
+            return 0;
+        }
     }
 }
