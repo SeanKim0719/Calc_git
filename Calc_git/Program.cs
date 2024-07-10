cfff4222fadddc5396626ed8e558a91580ebfec8
NDH <ndhphysics@gamil.com>
2024-07-10 20:19:10 +0900

    Calculator 초기 상태

--- Calc_git/Program.cs
+++ Calc_git/Program.cs
@@ -6,19 +6,11 @@ class Program
     static void Main(string[] args)
     {
         Calculator calc = new Calculator();
-        calc.display();
-        Console.WriteLine("git");
     }
 }
 
 class Calculator
 {
-    
-    public int CalcAdd(int num1, int num2)
-    {
-        return num1 + num2; 
-    }
-
     public void display()
     {
         Console.WriteLine("Calculate is booting");
